5e8f974e85fb447ad5c83f893ccc09a2c57a0956
Javier Pestana <javier.pestana85@gmail.com>
2024-09-12 09:48:50 +0100

    header, some global styles and budged component

---
+++ src/components/Budged.vue
@@ -0,0 +1,12 @@
+<script setup></script>
+
+<template>
+  <form>
+    <div class="campo">
+      <label for="new-budged">Define Budged</label>
+      <input id="new-budged" class="new-budged" placeholder="Add Your Budged" />
+    </div>
+    <input type="submit" value="Define Budged" />
+  </form>
+</template>
+<style scoped></style>
